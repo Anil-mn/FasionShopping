bc504bdf4b66e205a97864f0ebce0db1b91ef9cf
ANIL MN <amn.nmb@gmail.com>
2020-06-24 11:59:43 +0530

    Update auth.js

--- controller/auth.js
+++ controller/auth.js
@@ -58,3 +58,13 @@ exports.signin = (req,res)=> {
       })
 }
 
+
+
+exports.signout = (req, res) => {
+    res.clearCookie("token");
+    res.json({
+      message: "User signout successfully"
+    });
+  };
+  
+

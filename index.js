22dc9e30c34a3660e3b4f2f20b57a025b456d7ac
Anil-mn <amn.nmb@gmail.com>
2023-01-30 11:03:39 +0530

    jenkins file added

--- index.js
+++ index.js
@@ -57,5 +57,5 @@ app.use("/api", products);
 //app.use("/api/products",  products);
 
 //port 5000
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5002;
 app.listen(port, () => console.log(`App is running at ${port}`));
